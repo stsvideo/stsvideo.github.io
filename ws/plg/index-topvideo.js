2e2b2b8208ec31e70025e76db65a1045886f3cd3
sungbly1tsstt <114800438+sungbly1tsstt@users.noreply.github.com>
2023-03-03 17:16:58 +0800

    Delete ws directory

--- ws/plg/index-topvideo.js
+++
@@ -1,9 +0,0 @@
-var objmain = document.getElementById("main");
-var objtext = document.getElementById("firstText");
-var Numberno;
-function change(){
-	objtext.innerHtml="no video now";
-}
-while(){
-	setTimeout(change(),100);
-}
