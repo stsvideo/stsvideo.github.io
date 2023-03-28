dda08c488fe910908acdcfaca0c9ce9a469f4e60
sungbly1tsstt <114800438+sungbly1tsstt@users.noreply.github.com>
2023-03-28 19:03:03 +0800

    update2.0

---
+++ ws/plg/index-topvideo.js
@@ -0,0 +1,9 @@
+var objmain = document.getElementById("main");
+var objtext = document.getElementById("firstText");
+var Numberno;
+function change(){
+	objtext.innerHtml="no video now";
+}
+while(){
+	setTimeout(change(),100);
+}
