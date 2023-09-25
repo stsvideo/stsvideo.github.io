adbd13332bf53e5773f07bf8f584c64d6eebf69f
sungbly1tsstt <onily.code_sungbly_tsstt@outlook.com>
2023-09-25 19:38:49 +0800

    eu
    emergency updaṫe

--- lock/password/2.js
+++ lock/password/2.js
@@ -1,6 +1,9 @@
 function verify(password){
     if(password=="oiwjdiju82ndasubiudwnasijsbdiuniwnu83dhiabdbshc"){
-        alert("跳转后激活码为jbdwudxianaiuhwd 长按鼠标左键并拖动至文本变蓝后按下Ctrl+c可以复制 Ctrl+v可以黏贴");
+        alert("tm还偷 给老子回来 爆丸小子")
+    }
+    if(password=="mdkjnefjw93jficjsxniuzcnwmfiocnwidjwjaicnisnf"){
+        alert("跳转后激活码为iojdoj21eddsx 长按鼠标左键并拖动至文本变蓝后按下Ctrl+c可以复制 Ctrl+v可以黏贴");
         location.href="//stsvideo.github.io/video/2.html";
     }
     alert("V我50 给激活码 或者破解\f爆丸小子(确信)");
